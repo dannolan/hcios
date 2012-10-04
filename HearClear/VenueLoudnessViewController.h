61787da425a0885f7d9d10717b8c1a843ca19428
Dan Nolan <dan.james.nolan@gmail.com>
2012-10-04 18:13:51 +1000

    Volume View segue etc

---
+++ HearClear/VenueLoudnessViewController.h
@@ -0,0 +1,13 @@
+//
+//  VenueLoudnessViewController.h
+//  HearClear
+//
+//  Created by Dan Nolan on 4/10/12.
+//  Copyright (c) 2012 Dan Nolan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VenueLoudnessViewController : UITableViewController
+
+@end
